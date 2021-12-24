9a77552b6951e5be34b9aa19d3380b3c7383be52
Jacob C <jacob@yakovliam.com>
2021-12-23 21:33:32 -0500

    Modify deployment debug statments

--- docs/google-search-grabber.js
+++ docs/google-search-grabber.js
@@ -17,9 +17,22 @@ const IFrame = ({
         </iframe>
     )
 }
+
+function uniq(a) {
+    var prims = {"boolean":{}, "number":{}, "string":{}}, objs = [];
+
+    return a.filter(function(item) {
+        var type = typeof item;
+        if(type in prims)
+            return prims[type].hasOwnProperty(item) ? false : (prims[type][item] = true);
+        else
+            return objs.indexOf(item) >= 0 ? false : objs.push(item);
+    });
+}
+
 const SearchedImages = (props) => {
     console.log("Props: " + JSON.stringify(props));
-    let items = [...new Set(props.results)];
+    let items = uniq(props.results);
 
     console.log("Items: " + JSON.stringify(items));
 
