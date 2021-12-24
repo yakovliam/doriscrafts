d57aaaf2b88a2b9648de22e86fc99977f38712f8
yakovliam <yakovliam@users.noreply.github.com>
2021-12-24 02:11:13 +0000

    Deploying to gh-pages from @ yakovliam/doriscrafts@052e6f65aad2b0e858dfff9ef13362cc88c97cb0 🚀

---
+++ assets/js/7254c19a.8247ccaa.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdoriscrafts=self.webpackChunkdoriscrafts||[]).push([[937],{1674:function(e){e.exports=JSON.parse('{"title":"Alphabet Index","slug":"/category/alphabet","permalink":"/docs/category/alphabet","navigation":{"previous":{"title":"Search All","permalink":"/docs/all"},"next":{"title":"Letter A","permalink":"/docs/alphabet/lettera"}}}')}}]);
---
+++ assets/js/b2f554cd.457bc366.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdoriscrafts=self.webpackChunkdoriscrafts||[]).push([[477],{10:function(s){s.exports={blogPosts:[]}}}]);
